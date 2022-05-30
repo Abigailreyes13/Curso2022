74a5aa7028cbda13fb0ddd25e75ff0d46b0b6d12
Abigailreyes13 <abigailreyes1318@gmail.com>
2022-05-29 22:10:56 -0300

    tarea 8

---
+++ Clase7/Practica/Program.cs
@@ -0,0 +1,91 @@
+﻿//Escribe un programa que:
+
+//1) Pida 10 números al usuario.
+//2) Obtener la suma de todos los números ingresados.
+//3) Obtener cual es el mayor de todos los números.
+//4) Obtener cual es el menor de todos los números.
+//5) Obtener el promedio de todos los números.
+//6) Mostrar todos los números ingresados por pantalla.
+//7) Mostrar los valores calculados en los puntos 2, 3, 4 y 5.
+
+//Recuerden que no nos deben adjuntar archivos comprimidos ni imagenes.
+//Pueden adjuntarnos el archivo .cs, o el codigo como texto.
+
+int[] numeros = new int[10];
+Console.WriteLine("Ingrese 10 números");
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Ingrese Numero: {i+1} ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+// obtener la suma de todos los números ingresados
+
+int suma = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+        suma = suma + numeros[i];
+}
+Console.WriteLine($"La suma de todos los números es: {suma}");
+
+// Obtener cual es el mayor de todos los números.
+
+int númeromayor = numeros[0];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (numeros[i] > númeromayor)
+    {
+        númeromayor = numeros[i];
+    }
+}
+Console.WriteLine($"El número mayor de todos es {númeromayor}");
+
+//Obtener cual es el menor de todos los números.
+int númeroMenor = numeros[0];
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (númeroMenor < numeros[0])
+    {
+        númeroMenor = numeros[0];
+    }
+}
+Console.WriteLine($"El número menor de todos es {númeroMenor}");
+
+//Obtener el promedio de todos los números.
+
+double promedio = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    promedio += numeros[i];
+    
+}
+promedio = promedio / numeros.Length;
+Console.WriteLine($"El promedio es: {promedio}");
+
+
+//Mostrar todos los números ingresados por pantalla.
+
+Console.WriteLine($"Los numeros ingresados son:");
+
+for (int i = 0; i < numeros.Length; i++)
+{
+   
+    Console.WriteLine(numeros[i]);
+}
+
+//Mostrar los valores calculados en los puntos 2, 3, 4 y 5.
+
+Console.WriteLine("los valores calculados en los puntos 2, 3, 4 y 5. son: ");
+
+for (int i = 2; i < numeros.Length; i = i + 2)
+{
+    Console.WriteLine(numeros[i]);
+}
+
+
+//Me siento feliz 
---
+++ Clase8/Práctica/Program.cs
@@ -0,0 +1,71 @@
+﻿//Ejercicio
+
+//Crear un programa que cumpla con los siguientes pasos
+
+//1) Crear una matriz de dos dimensiones de tipo int llamada numeros,
+//2) Determinar el tamaño de cada dimension (fila, columnas) mediante valores ingresados por pantalla
+//3) Declarar un vector de tipo double llamado promedios
+//4) Recorrer la matriz para su carga con elementos de tipo int
+//5) Recorrer la matriz para mostrar cada valor de la matriz
+//6) Calcular el promedio de cada columna y asignarlo a la posicion correspondiente dentro del vector promedios
+//7) Mostrar los promedios recorriendo el vector promedios
+
+
+
+Console.WriteLine("Ingrese cantidad de alumnos");
+int cantidadDeAlumnos = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese cantidad de notas para cada alumno");
+int cantidadDeNotas = int.Parse(Console.ReadLine());
+
+double [] promedios = new double[cantidadDeAlumnos];
+
+int[,] numeros = new int[cantidadDeNotas, cantidadDeAlumnos];
+
+int lengthFilas = numeros.GetUpperBound(0) + 1;
+int lengthColumnas = numeros.GetUpperBound(1) + 1;
+
+//Recorrer la matriz para ingresas valores
+
+for (int columna = 0; columna < lengthColumnas; columna++)
+{
+         
+    Console.WriteLine($"Ingrese las notas del alumno: {columna + 1}: ");
+
+    for (int Fila = 0; Fila < lengthFilas; Fila++)
+    {
+        Console.WriteLine($"cargue la nota N° {Fila + 1}");
+        numeros[Fila, columna] = int.Parse(Console.ReadLine());
+    }
+
+}
+
+// Recorrer la matriz para mostrar valores 
+
+for (int columna = 0; columna < lengthColumnas; columna++)
+{
+    Console.WriteLine("==========================");
+
+    Console.WriteLine($"Las notas del alumno {columna + 1} son: ");
+
+    for (int Fila = 0; Fila < lengthFilas; Fila++)
+    {
+        Console.WriteLine($"La nota N° {Fila + 1}");
+        Console.WriteLine(numeros[Fila, columna]);
+    }
+    
+}
+
+// Calcular promedios
+for (int columa = 0; columa < lengthColumnas; columa++)
+{
+    Console.WriteLine("==========================");
+    Console.WriteLine($"El promedio de las notas del alumno {columa + 1} es:");
+
+    double suma = 0; 
+    for (int Fila = 0; Fila < lengthFilas; Fila++)
+    {
+       suma = suma + numeros[Fila, columa];
+    }
+    Console.WriteLine($"{promedios[columa] = suma / lengthFilas}");
+}
